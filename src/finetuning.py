7b4be26df081255f0a12ce0824f386ff62948954
felixjordan <felix.jordan@web.de>
2025-03-19 20:44:00 +0100

    updated query generation

--- src/finetuning.py
+++ src/finetuning.py
@@ -1,4 +1,5 @@
 import os
+from typing import List
 
 from groq import Groq
 from sentence_transformers import SentenceTransformer
@@ -22,12 +23,19 @@ class Finetuner:
             embedding_model_name,
             trust_remote_code=True,
         )
+        self.queries: List[str] = []
 
     def finetune(self):
-        queries = [self.generate_query(chunk) for chunk in self.chunks]
+        print("starting to finetune embedding model...")
+        self._generate_queries_for_chunks()
 
-    def generate_query(self, text):
-        prompt = f"Generate a natural language search query a user might use to find this text:\n{text}\n\nQuery:"
+    def _generate_query(self, text):
+        prompt = (
+            f"Erzeuge eine Anfrage in natürlicher Sprache, die ein Nutzer stellen würde, "
+            f"um den folgenden TEXT zu finden. Antworte auschließlich mit der Anfrage ohne weitere Informationen. "
+            f"Antworte mit nur genau einer Anfrage. "
+            f"Der TEXT lautet:\n{text}"
+        )
         chat_completion = self.client.chat.completions.create(
             messages=[
                 {
@@ -41,3 +49,21 @@ class Finetuner:
         )
 
         return chat_completion.choices[0].message.content
+
+    def _generate_queries_for_chunks(self):
+        print(f"generating queries for {len(self.chunks)} chunks")
+        self.queries = [self._generate_query(chunk) for chunk in self.chunks]
+
+
+if __name__ == "__main__":
+    f = Finetuner(
+        path_chunked_docs="chunked_documents.json",
+        embedding_model_name="jinaai/jina-embeddings-v2-base-de",
+    )
+    f.finetune()
+    for i in range(len(f.chunks)):
+        print(f"query: {f.queries[i]}")
+        print()
+        print(f"chunk: {f.chunks[i]}")
+        print()
+        print()
--- src/utils.py
+++ src/utils.py
@@ -2,12 +2,13 @@ import json
 from typing import List
 
 
-def load_chunks(path_chunks_json) -> List:
+def load_chunks(path_chunks_json) -> List[str]:
     """
     Loads chunks from json file and returns them as list of strings.
     :param path_chunks_json: path to json file with chunks
     :return: list of all chunks
     """
+    print(f"loading chunks from {path_chunks_json}...")
     with open(path_chunks_json, "r") as f:
         chunks_dict = json.load(f)
     chunks = [chunk["text"] for chunk in chunks_dict]
